8fa9f47386927119cf9c9b19bad8d78a700f19dd
anika-jane <112495457+anika-jane@users.noreply.github.com>
2023-04-18 11:05:53 -0400

    added the form and loop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        hello
+      <div className="container">
+      <header className="#">
       </header>
+      <main>
+        <Dictionary />  
+      </main>
+      <footer className="text-center">
+        <smaller>Coded by Anika Jane</smaller></footer>
+      </div>
     </div>
   );
 }
