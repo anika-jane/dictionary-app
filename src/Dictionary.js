cda6e1096688eeab10e2f1250fcc5586211a5628
anika-jane <112495457+anika-jane@users.noreply.github.com>
2023-04-19 10:36:18 -0400

    updated responses

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results"
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data[0]);
+       setResults(response.data[0]);
     }
 
     function search(event) {
         event.preventDefault();
 
-        let key = "f2dbddf916eb4bcoaf1d74404tf15c3b";
-        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
     }
 
@@ -23,9 +24,11 @@ export default function Dictionary() {
         setKeyword(event.target.value);
     }
 
-    return <div className="Dictionary">
+    return (<div className="Dictionary">
         <form onSubmit={search}>
             <input type="search" onChange={handleKeywordChange} />
         </form>
-    </div>;
+        <Results results={results} />
+    </div>
+    );
 }
