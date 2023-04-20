049f0b0e69aeeb964de54b212914f2b20ee79e81
anika-jane <112495457+anika-jane@users.noreply.github.com>
2023-04-20 10:54:23 -0400

    Added phonetics

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 export default function Synonyms(props) {
-    if (props.synonyms){
-        return (
-            <ul className="synonyms">
-            {props.synonyms.map(function (synonyms, index){
-                return <li><span key={index}>{synonyms}</span></li>;
-            })}
-            </ul>
-        );
-} else {
+  if (props.synonyms) {
+    return (
+      <ul className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
+        })}
+      </ul>
+    );
+  } else {
     return null;
-}
+  }
 }
